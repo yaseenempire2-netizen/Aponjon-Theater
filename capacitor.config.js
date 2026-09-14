f6075c417aab0701036e36881adbb097db40878a
yaseenempire2-netizen <yaseenempire2@gmail.com>
2026-09-14 16:25:17 +0600

    Create capacitor.config.js

---
+++ capacitor.config.js
@@ -0,0 +1,11 @@
+const config = {
+  appId: 'com.yaseenahmed.aponjontheater',
+  appName: 'Aponjon Theater',
+  webDir: 'client/dist',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+module.exports = config;
